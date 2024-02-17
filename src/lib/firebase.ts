10a760f26c9ad5e429006a8c3088925a34fc44b9
Bas [Panwan] <panwan.21st@gmail.com>
2024-02-18 00:45:56 +0700

    env update

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,20 +2,23 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import * as dotenv from 'dotenv';
+dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+console.log(process.env.NEXT_PUBLIC_API_KEY);
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCyC16mgj1pc0rVGgYTICSIsRg3ZxQRK1o",
-  authDomain: "qualified-abode-411319.firebaseapp.com",
-  databaseURL: "https://qualified-abode-411319-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "qualified-abode-411319",
-  storageBucket: "qualified-abode-411319.appspot.com",
-  messagingSenderId: "190098194498",
-  appId: "1:190098194498:web:238f19d91cd22535a12ddb",
-  measurementId: "G-TDJHGGZ5M1"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_API_KEYAUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_API_KEYDATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_API_KEYPROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_API_KEYSTORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_API_KEYMESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_API_KEYAPP_ID,
+  measurementId: process.env.NEXT_PUBLIC_API_KEYMEASUREMENT_ID
 };
 
 // Initialize Firebase
